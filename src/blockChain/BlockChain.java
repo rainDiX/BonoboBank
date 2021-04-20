17327765a0b81666c50fce4668dc8c9af8489fea
Alvenicum <alvenicum@gmail.com>
2021-04-20 03:38:45 +0200

    add block finished

--- src/blockChain/BlockChain.java
+++ src/blockChain/BlockChain.java
@@ -2,6 +2,7 @@ package blockChain;
 
 import java.util.LinkedList;
 
+
 public class BlockChain {
     /**
      * Difficulté de la blockchain, içi le nombre de 0 par lequel
@@ -17,8 +18,15 @@ public class BlockChain {
         this.difficulty = difficulty;
     }
 
-    public int addBlock() {
-        return 0;
+    public boolean addBlock(Block b) {
+    	if (b.getHash().startsWith("0".repeat(this.difficulty)) && b.getPrevBlockHash()==blockList.getLast().getPrevBlockHash()) {
+    		blockList.add(b);
+    		return true;
+    	}
+    	
+        return false;
     }
+    
+    
 
 }
