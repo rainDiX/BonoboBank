c2edff89d53906fdfa55af276dc4d13dcfdc0672
Raimu <lime@raimu.moe>
2021-04-30 19:59:04 +0200

    Add flags to main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import bcb.CentralBank;
 
-
 public class Main {
-    public static void launchBCB(int difficulty, int userCount, int blockCount, long initialReward) {
+    public static void launchBCB(int difficulty, int userCount, int blockCount, long initialReward, boolean saving, String filename) {
         System.out.println("Difficulté du minage             : " + difficulty);
         System.out.println("Nombre d'utilisateur             : " + userCount);
         System.out.println("Nombre de block à miner          : " + blockCount);
@@ -20,7 +19,14 @@ public class Main {
         // Phase de marché :
         coinbase.mercatoPhase(blockCount);
 
-        coinbase.writeJson("./BC.json");
+        if (saving) {
+            coinbase.writeJson(filename);
+        }
+    }
+
+    public static void openBCB(String filename) {
+        CentralBank bcb = new CentralBank(filename);
+        System.out.println(bcb.getName());
     }
 
     public static void printUsage() {
@@ -29,57 +35,77 @@ public class Main {
         System.out.println("-d difficulté : difficulté du minage (4 par défaut)");
         System.out.println("-r montant : Montant initial en Bnb recu pendant d'helicopter Money (50 Bnb par défaut)");
         System.out.println("-u nb : nombre d'utilisateur (10 par défaut)");
+        System.out.println("-f fichier.json : fichier contenant une blockchain");
+        System.out.println("-s fichier.json : fichier où sauvegarder la blockchain à la fin");
     }
 
     public static void main(String[] args) {
+        boolean opening = false;
+        boolean saving = false;
+        boolean helpflag = false;
+        String filename = "";
         int blockCount = 100;
         int difficulty = 4;
         // 50 Bnb
         long initialReward = 5000000000l;
         // nombre d'utilisateur
         int userCount = 10;
-        
+
         for (int i = 0; i < args.length - 1; ++i) {
             switch (args[i]) {
-            case "-b":
-                try {
-                    blockCount = Integer.parseUnsignedInt(args[i + 1]);
-                    ++i;
-                } catch (NumberFormatException ex) {
-                    System.out.println("le nombre de block n'est pas fournis ou invalide");
-                    System.out.println("utilisation de la valeur par défaut");
-                }
-                break;
-            case "-d":
-                try {
-                    difficulty = Integer.parseUnsignedInt(args[i + 1]);
-                    ++i;
-                } catch (NumberFormatException ex) {
-                    System.out.println("la difficulté n'est pas fournis ou invalide");
-                    System.out.println("utilisation de la valeur par défaut");
-                }
-                break;
-            case "-r":
-                try {
-                    int montantBnb = Integer.parseUnsignedInt(args[i + 1]);
-                    initialReward = montantBnb * 100000000l;
-                    ++i;
-                } catch (NumberFormatException ex) {
-                    System.out.println("le montant n'est pas fournis ou invalide");
-                    System.out.println("utilisation de la valeur par défaut");
-                }
-                break;
-            case "-u":
-                try {
-                    userCount = Integer.parseUnsignedInt(args[i + 1]);
-                    ++i;
-                } catch (NumberFormatException ex) {
-                    System.out.println("le nombre d'utilisateur n'est pas fournis ou invalide");
-                    System.out.println("utilisation de la valeur par défaut");
-                }
-                break;
+                case "-b":
+                    try {
+                        blockCount = Integer.parseUnsignedInt(args[i + 1]);
+                        ++i;
+                    } catch (NumberFormatException ex) {
+                        System.out.println("le nombre de block n'est pas fournis ou invalide");
+                        System.out.println("utilisation de la valeur par défaut");
+                    }
+                    break;
+                case "-d":
+                    try {
+                        difficulty = Integer.parseUnsignedInt(args[i + 1]);
+                        ++i;
+                    } catch (NumberFormatException ex) {
+                        System.out.println("la difficulté n'est pas fournis ou invalide");
+                        System.out.println("utilisation de la valeur par défaut");
+                    }
+                    break;
+                case "-r":
+                    try {
+                        int montantBnb = Integer.parseUnsignedInt(args[i + 1]);
+                        initialReward = montantBnb * 100000000l;
+                        ++i;
+                    } catch (NumberFormatException ex) {
+                        System.out.println("le montant n'est pas fournis ou invalide");
+                        System.out.println("utilisation de la valeur par défaut");
+                    }
+                    break;
+                case "-u":
+                    try {
+                        userCount = Integer.parseUnsignedInt(args[i + 1]);
+                        ++i;
+                    } catch (NumberFormatException ex) {
+                        System.out.println("le nombre d'utilisateur n'est pas fournis ou invalide");
+                        System.out.println("utilisation de la valeur par défaut");
+                    }
+                    break;
+                case "-s":
+                    filename = args[i+1];
+                    saving = true;
+                    break;
+                case "-f":
+                    filename = args[i+1];
+                    opening = true;
+                    break;
             }
         }
-        launchBCB(difficulty, userCount, blockCount, initialReward);
+        if ( helpflag || (opening && saving)){
+            printUsage();
+        } else if (opening) {
+            openBCB(filename);
+        } else {
+            launchBCB(difficulty, userCount, blockCount, initialReward, saving, filename);
+        }
     }
 }
